400901a94d548e57114c94a0c8afdaca8edc51c8
eyup.yigit <eyup.yigit@DESKTOP-QCN50RO>
2023-02-28 16:24:31 +0300

    Entities class

---
+++ Core/Context/DbContext.cs
@@ -0,0 +1,18 @@
+﻿using Core.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Core.Context
+{
+    public class DbContext : IdentityDbContext<AppUser, AppRole, int>
+    {
+        public DbContext(DbContextOptions<DbContext> options) : base(options)
+        {
+
+        }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+    }
+}
