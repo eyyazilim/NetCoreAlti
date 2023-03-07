7da48c8fb8f66a1a377fdf1c7ee3655414021ee8
eyup.yigit <eyup.yigit@DESKTOP-QCN50RO>
2023-03-07 09:43:16 +0300

    DATABASE CONNECTİON

--- Api/Program.cs
+++ Api/Program.cs
@@ -1,3 +1,6 @@
+using Core.Context;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -7,7 +10,13 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddDbContext<AppDbContext>(opt =>
+{
+    opt.UseSqlServer("server=.\\SQLEXPRESS; database=EyNetCoreAltiDb; integrated security=true;");
+});
+
 var app = builder.Build();
+
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
---
+++ Core/Context/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Core.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+
+namespace Core.Context
+{
+    public class AppDbContext : IdentityDbContext<AppUser, AppRole, int>
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+    }
+}
